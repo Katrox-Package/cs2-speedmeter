4dea9977cdd396ca202facab291d65742f928347
RoxyRekt <87309991+roxyrekt@users.noreply.github.com>
2025-03-11 00:39:14 +0300

    0.0.3

--- Main.cs
+++ Main.cs
@@ -44,7 +44,7 @@ namespace SpeedMeter
     {
         public override string ModuleName => "cs2-speedmeter";
         public override string ModuleAuthor => "Roxy & Katarina";
-        public override string ModuleVersion => "0.0.2";
+        public override string ModuleVersion => "0.0.3";
 
         public override void Load(bool hotReload)
         {
